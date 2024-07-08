3e7830217dcb0e8b6efb8540b94e701f39eff2d5
Alessandro Tigani <alessandrotigani@gmail.com>
2024-07-08 12:29:43 +0200

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1MjAyNA/spectral.js?branch_slug=main&token=0487762d-6e2b-48cd-a3bd-89ef24f100b3";
+export default { extends: ruleset };
