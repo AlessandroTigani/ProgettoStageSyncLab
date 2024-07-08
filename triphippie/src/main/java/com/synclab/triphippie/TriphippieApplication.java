e33919b8008b19b8b13b1f7b68355edee0634989
Tigo <alessandrotigani@gmail.com>
2024-07-08 13:03:37 +0200

    caricamento progetto iniziale

---
+++ triphippie/src/main/java/com/synclab/triphippie/TriphippieApplication.java
@@ -0,0 +1,13 @@
+package com.synclab.triphippie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriphippieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriphippieApplication.class, args);
+	}
+
+}
---
+++ triphippie/src/test/java/com/synclab/triphippie/TriphippieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synclab.triphippie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriphippieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
